6ef46b954f1f123e091dd35b0f27a4641759c293
Matthew D Diaz <matthewdiaz10@ufl.edu>
2015-12-31 14:02:34 -0500

    Modify images for Carosuel in home.html

--- public/js/controllers/communityController.js
+++ public/js/controllers/communityController.js
@@ -1,24 +1,26 @@
 angular.module('projectHope.communityController', ['ngRoute', 'ui.materialize'])
   .controller('CommunityController', ['$http', '$scope', function($http, $scope){
     $scope.acmPrinciples = [
+
+
       {
-          "title" : "Experience God",
-          "description" : "Description 1 goes here",
+          "title" : "Experiencing God",
+          "description" : "I believe each of these values reflects what we aim to accomplish for ourselves and for others. That said, experiencing God means we seek to feel God's presence amongst us and within our individual daily lives and assist others to feel his presence as well. ",
           "videoURL" : "/videos/video1.mp4"
       },
       {
           "title" : "Sharing Love",
-          "description" : "Description 2 goes here",
+          "description" : "Sharing love means that we genuinely for one another and display acts of affection towards one another so that we can all feel the joy that comes from being loved. ",
           "videoURL" : "/videos/video1.mp4"
       },
       {
           "title" : "Connecting Lives",
-          "description" : "Description 3 goes here",
+          "description" : "Connecting lives means we come here maybe without knowing anyone or with only a few acquaintances but we gain new friends and make connections that can change our lives.",
           "videoURL" : "/videos/video1.mp4"
       },
       {
           "title" : "Declaring Truth",
-          "description" : "Description 4 goes here",
+          "description" : "Declaring truth means we share the adventist truths and the love of jesus with all whom we interact.",
           "videoURL" : "/videos/video1.mp4"
       }
     ]
