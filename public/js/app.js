c581e1c47afdb5b627335539122aa7f5c43d2e6b
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-01-09 19:49:19 -0500

    Delete navigationController.js and navBarSrvice.js. Unnecessary classes. Removed navBarShrink function from app.js and moved it to its own file called navBarShrink.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 (function(){
   var app = angular.module('projectHope', ['ngRoute', 'ui.materialize',
-                          'projectHope.communityController',
-                          'projectHope.navigationController']);
+                          'projectHope.communityController']);
 
   app.config(['$routeProvider', function($routeProvider){
 		$routeProvider//Are the specific routes that user types in the browser will inject the specifc html to index.html
@@ -23,32 +22,5 @@
       $(".button-collapse").sideNav();
    });
 
-   app.service('navBarService', ['$rootScope', function($rootScope){
-      // var ser = this;
-    $rootScope.fullSize = true;
-     $(document).ready(function(){
-       $(document).scroll(function(){
-         var scrollDistance = $(this).scrollTop();
-         if(scrollDistance > 76){
-          //  $rootScope.$apply();
-          //  $rootScope.fullSize = false;
-           $('#acm-nav').addClass('shrunk-size-nav');
-           $('#acm-nav').removeClass('full-size-nav');
-           $('.acm-social-media-links').addClass('hide-container');
-           $('.left').removeClass('hide-container');
-           $('.brand-logo').addClass('center');
-         }else{
-          //  $rootScope.$apply();
-          //  $rootScope.fullSize = true;
-           $('#acm-nav').removeClass('shrunk-size-nav');
-           $('.acm-social-media-links').removeClass('hide-container');
-           $('#acm-nav').addClass('full-size-nav');
-           $('.left').addClass('hide-container');
-           $('.brand-logo').removeClass('center');
-         }
-       });
-     });
-    //  this.getData = function() { return $rootScope.fullSize; };
-   }]);
-
+   shrinkNavBar();
 })();
