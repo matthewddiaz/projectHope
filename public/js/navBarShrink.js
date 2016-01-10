55b79dfd743d80481a87fa91bf41463cf8f9c0eb
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-01-10 12:17:23 -0500

    Modify acm logo in shrunk navigation mode

--- public/js/navBarShrink.js
+++ public/js/navBarShrink.js
@@ -8,12 +8,14 @@ function shrinkNavBar() {
         $('.acm-social-media-links').addClass('hide-container');
         $('.left').removeClass('hide-container');
         $('.brand-logo').addClass('center');
+        $('.brand-logo img').addClass('shrunk-logo-style');
       }else{
         $('#acm-nav').removeClass('shrunk-size-nav');
         $('.acm-social-media-links').removeClass('hide-container');
         $('#acm-nav').addClass('full-size-nav');
         $('.left').addClass('hide-container');
         $('.brand-logo').removeClass('center');
+        $('.brand-logo img').removeClass('shrunk-logo-style');
       }
     });
   });
