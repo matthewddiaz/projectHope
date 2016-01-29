65c90fd479e5df96b47f3fa7eb71ffc4ec219d95
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-01-29 16:08:57 -0500

    Add resolve to all of the paths foracmuf.org. Forces all pages to start at the top of the page. Giving the use the desired affect.

--- public/js/app.js
+++ public/js/app.js
@@ -6,27 +6,61 @@
                           'projectHope.navigationController']);
 
   app.config(['$routeProvider', function($routeProvider){
-		$routeProvider//Are the specific routes that user types in the browser will inject the specifc html to index.html
+		$routeProvider//Are the specific routes that user types in the browser, injects the specifc html to index.html
       .when('/blog',{
-        templateUrl : 'blog.html'
+        templateUrl : 'blog.html',
+        resolve: {
+          topOfPage : scrollTop
+        }
       })
       .when('/community', {
-        templateUrl : 'community.html'
+        templateUrl : 'community.html',
+        resolve: {
+          topOfPage : scrollTop
+        }
       })
       .when('#experiencingGod', {
-        templateUrl : 'community.html'
+        templateUrl : 'community.html',
+        resolve: {
+          topOfPage : scrollTop
+        }
       })
       .when('/events', {
-        templateUrl : 'events.html'
+        templateUrl : 'events.html',
+        resolve: {
+          topOfPage : scrollTop
+        }
       })
       .when('/home', {
-				templateUrl : 'home.html'
+				templateUrl : 'home.html',
+        resolve: {
+          topOfPage : scrollTop
+        }
 			})
 			.otherwise({
-				redirectTo: '/home'
+				redirectTo: '/home',
+        resolve: {
+          topOfPage : scrollTop
+        }
 			});
 	}]);
 
+  /*    NOTE: The scrollTop solves the following scenario
+   *    When pages change and user has scrolled down on that
+   *    page. Now clicking on another page in the navigation
+   *    bar will start the page at the same height where the user
+   *    was on the previous page.
+   *
+   *    This function scrolls 10000px upward at the start of every
+   *    page.
+   */
+  var scrollTop = function(){
+    $('html, body').animate({ scrollTop: -10000 }, 100);
+  }
+
+  /*  NOTE: For Mobile version only
+   *  Increased the width of the navigation side bar.
+   */
    $('.button-collapse').sideNav({
       closeOnClick: true, // Closes side-nav on <a> clicks
       menuWidth: 300 // Default is 240
